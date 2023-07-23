accf22714bcc7d220711367337c8c5026262d641
vytdev <123163428+vytdev@users.noreply.github.com>
2023-07-23 09:02:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Debug Stick
+
+This add-on implements a debug stick based on Minecraft: Java Edition.
+
+### Features
+
+Like in Java, you can do:
+
+- Change block properties
+- Update property values
+- See messages on actionbar like in Minecraft Java
+- Tested and working properly with Minecraft: Bedrock Edition 1.20.01
+
+### How to get
+
+- Download the add-on at the [releases section][releases], or directly [here][dl]
+- Import the add-on to Minecraft
+- Activate it in your world
+- Turn on Experimental Beta APIs
+- Open your world
+- Type the following in the chat commands:
+    
+    ```text
+    /give @s vyt:debug_stick
+    ```
+
+### Limitations
+
+I've also encountered some bugs while developing the add-on:
+
+- Some block properties like in Java are not available
+- Since the add-on listens for block breaks event, some blocks when breaked do
+  their expected behaviour. It will be fixed once the `EntityHitBlockAfterEvent`
+  were available.
+
+### Contributing
+
+Feel free to contribute with the add-on.
+
+### License
+
+Copyright &copy; 2023 Vincent Yanzee J. Tan. Licensed under the MIT License.
+
+[releases]: https://github.com/vytdev/debug-stick/releases
+[dl]: https://github.com/vytdev/debug-stick/releases/latest/download/debug-stick.mcpack
---
+++ src/scripts/index.js
@@ -0,0 +1,95 @@
+import { BlockStates, world, system } from "@minecraft/server";
+
+const record = {};
+
+function message(msg, player) {
+    return player.runCommandAsync(`title @s actionbar ${msg}`);
+}
+
+// listen for interaction with blocks
+// BUG:  when you break nbt blocks (like chest), nbt will be lost
+// This will be fixed once EntityHitBlockAfterEvent available
+world.afterEvents.blockBreak.subscribe((ev) => {
+    const player = ev.player;
+    const heldItem = player.getComponent("minecraft:inventory")
+        .container.getItem(player.selectedSlot);
+    if (heldItem?.typeId != "vyt:debug_stick")
+        return;
+    // block permutation
+    const block = ev.block;
+    const permutation = ev.brokenBlockPermutation;
+    // put the block back
+    block.setPermutation(permutation);
+    // get next state
+    const states = permutation.getAllStates();
+    const names = Object.keys(states);
+    // check block
+    if (!names.length && !block.type.canBeWaterlogged)
+        return message(`${block.typeId} has no properties`, player);
+    // retrieve state
+    let prop = names[names.indexOf(record[player.id]) + 1];
+    let val = states[prop];
+    // loop through properties
+    if (!prop) {
+        // waterlogged property available
+        if (block.type.canBeWaterlogged) {
+            prop = "waterlogged";
+            val = block.isWaterlogged;
+        }
+        // reset to 0
+        else {
+            prop = names[0];
+            val = states[prop];
+        }
+    }
+    // update record
+    record[player.id] = prop;
+    // send message
+    message(`selected "${prop}" (${val})`, player);
+});
+
+// listen for clicks in blocks
+world.beforeEvents.itemUseOn.subscribe((ev) => {
+    // check for player and his held item
+    if (ev.source.typeId != "minecraft:player" || ev.itemStack?.typeId != "vyt:debug_stick")
+        return;
+    // cancel event behaviour
+    ev.cancel = true;
+    const player = world.getAllPlayers().find(v => v.id == ev.source.id);
+    // block permutation
+    const block = ev.block;
+    const permutation = block.permutation;
+    // get all block states/properties
+    const states = permutation.getAllStates();
+    const names = Object.keys(states);
+    // check block
+    if (!names.length && !block.type.canBeWaterlogged)
+        return message(`${block.typeId} has no properties`, player);
+    // retrieve property name
+    let prop = record[player.id];
+    let val;
+    // check if recorded state are available in the block
+    if (prop == "waterlogged" ? !block.type.canBeWaterlogged : !names.includes(prop))
+        prop = names[0];
+    // toggle waterlogged
+    if (prop == "waterlogged") {
+        val = !block.isWaterlogged;
+        system.run(() => {
+            block.isWaterlogged = val;
+        });
+    }
+    // a property
+    else {
+        const valids = BlockStates.get(prop).validValues;
+        val = valids[valids.indexOf(states[prop]) + 1];
+        // loop through values
+        if (typeof val === "undefined")
+            val = valids[0];
+        // update property
+        system.run(() => {
+            block.setPermutation(permutation.withState(prop, val));
+        });
+    }
+    // send message
+    message(`"${prop}" to ${val}`, player);
+})
