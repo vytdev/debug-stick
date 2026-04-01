81fb016269c696b2225ca70a1148801b9b2345c8
Vincent Yanzee J. Tan <numsqx@proton.me>
2026-04-01 22:08:52 +0800

    add: rough proof-of-concept implementation

--- src/ui-handlers.ts
+++ src/ui-handlers.ts
@@ -7,18 +7,66 @@
  * See LICENSE for the full terms.
  */
 
-import { Block, Player } from '@minecraft/server';
+import { Block, BlockPermutation, Player, system } from '@minecraft/server';
+import { ModalFormData } from '@minecraft/server-ui';
 import { message } from './utils.js';
-import { getStatesOfBlock, getStateValidValues, setBlockState
-    } from './state.js';
-import { DebugStateSelections } from './selection.js';
+import { getStatesOfBlock, getStateValidValues } from './state.js';
+import type { StateName, StateValue } from './types';
 
+
+// TODO: clean all of these
 
 /**
  * Open the Debug Stick UI form.
  * @param player The player to open the UI for.
  * @param block The working block.
  */
-export function openUI(player: Player, block: Block) {
-  message('Not yet implemented.', player);
+export function openUI(player: Player, block: Block): void {
+  const typeId = block.typeId;
+  const currStates = getStatesOfBlock(block);
+  const stateNameList = Object.keys(currStates);
+  const form = new ModalFormData();
+
+  form.title('§6Debug Stick UI§r');
+  form.label('This is an experimental feature.\n' +
+             'Debug Stick v26.10.1-beta8');
+  form.label(`Modifying states of block: §l§b${typeId}§r`);
+
+  for (const stateName of stateNameList) {
+    addStateToForm(form, stateName, currStates[stateName]);
+  }
+
+  form.label('You can click the §cX§r button or press §cEsc§r ' +
+             'if you wish to cancel this operation.');
+  form.submitButton('Apply Changes');
+
+  system.run(() => {
+    form.show(player).then(r => {
+      if (r.canceled || !r.formValues) return;
+      const newStateObj: any = {};
+
+      let i = 0;
+      for (const stateName of stateNameList) {
+        const validVals = getStateValidValues(stateName);
+        const selVal = validVals[r.formValues[i++ + 2] as number];
+        newStateObj[stateName] = selVal;
+      }
+
+      delete newStateObj['waterlogged'];  // temporary
+      block.setPermutation(BlockPermutation.resolve(typeId, newStateObj));
+
+      // DEBUG
+      message(JSON.stringify(newStateObj), player);
+    })
+  });
+}
+
+
+export function addStateToForm(form: ModalFormData, stateName: StateName,
+                               currValue: StateValue): void
+{
+  const validVals = getStateValidValues(stateName);
+  form.dropdown(stateName, validVals.map(String), {
+    defaultValueIndex: validVals.indexOf(currValue),
+  });
 }
