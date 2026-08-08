116232e2d034612dc2b5ba4ce27659fae6f4da64
Vincent Yanzee J. Tan <vytdev@gmail.com>
2026-08-09 06:51:26 +0800

    bump: 26.40.0 (2.9.0)

--- config.js
+++ config.js
@@ -26,12 +26,12 @@ export default {
   /**
    * The current version.
    */
-  packVersion: '26.30.0',
+  packVersion: '26.40.0',
 
   /**
    * Minimum Minecraft version required.
    */
-  minMcVersion: '1.26.30',
+  minMcVersion: '1.26.40',
 
   /**
    * Script entry point.
@@ -56,7 +56,7 @@ export default {
    * Dependencies.
    */
   dependencies: {
-    '@minecraft/server': '2.8.0',
+    '@minecraft/server': '2.9.0',
     //'@minecraft/server-ui': '2.0.0',
   }
 };
